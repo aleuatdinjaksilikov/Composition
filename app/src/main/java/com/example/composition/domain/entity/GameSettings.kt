eda87763f4f947cd2e9caff250134e2366c50161
alewatdinjaksilikov <jaksilikovalewatdin2004@gmail.com>
2022-10-20 23:23:09 +0500

    design,arguments,parcelable,fragments

--- app/src/main/java/com/example/composition/domain/entity/GameSettings.kt
+++ app/src/main/java/com/example/composition/domain/entity/GameSettings.kt
@@ -1,8 +1,12 @@
 package com.example.composition.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class GameSettings (
     val maxSumValue:Int, //Макс. возможное зн. суммы
     val minCountOfRightAnswers:Int, //Мин. кол-во правильных ответов для победы
     val minPercentOfRightAnswers:Int, //Мин. процент правильных ответов
     val gameTimeInSeconds:Int //Время игры в секундах
-        )
+        ):Parcelable
